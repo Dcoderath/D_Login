4b56b0f2779a36cd78888512735605314ddadbbd
dcoderath <trivedi2693@gmail.com>
2025-06-27 23:39:01 +0530

    Updated content for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-// src/App.jsx
+// // src/App.jsx
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import ChatGPTLogin from "./components/ChatGPTLogin/ChatGPTLogin";
+// import Instagram from "./components/Instagram/Instagram";
+// import LoginDashboard from "./components/LoginDashboard/LoginDashboard";
+
+// const App = () => {
+//   return (
+//     <Router basename="/D_Login">
+//       <Routes>
+//         <Route path="/" element={<LoginDashboard />} />
+//         <Route path="/login" element={<ChatGPTLogin />} />
+//         <Route path="/instagram" element={<Instagram />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // ✅ Use HashRouter
 import ChatGPTLogin from "./components/ChatGPTLogin/ChatGPTLogin";
 import Instagram from "./components/Instagram/Instagram";
 import LoginDashboard from "./components/LoginDashboard/LoginDashboard";
 
 const App = () => {
   return (
-    <Router basename="/D_Login">
+    <Router>
       <Routes>
         <Route path="/" element={<LoginDashboard />} />
         <Route path="/login" element={<ChatGPTLogin />} />
