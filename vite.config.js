ebc1e903c4d694f39da7e0b198824fb2afa3a6b2
dcoderath <trivedi2693@gmail.com>
2025-06-27 16:41:43 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/D_Login/',
-  plugins: [react()]
+  base: '/D_Login/', // 👈 this is REQUIRED for GitHub Pages!
+  plugins: [react()],
 })
